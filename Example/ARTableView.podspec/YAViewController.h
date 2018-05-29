fb798378ddcf6af126775e63756078e106351336
onlyAngelia <479397375@qq.com>
2018-05-29 13:14:30 +0800

    pod lib

---
+++ Example/ARTableView.podspec/YAViewController.h
@@ -0,0 +1,13 @@
+//
+//  YAViewController.h
+//  ARTableView.podspec
+//
+//  Created by onlyAngelia on 05/29/2018.
+//  Copyright (c) 2018 onlyAngelia. All rights reserved.
+//
+
+@import UIKit;
+
+@interface YAViewController : UIViewController
+
+@end
---
+++ Example/ARTableView.podspec/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ARTableView.podspec
+//
+//  Created by onlyAngelia on 05/29/2018.
+//  Copyright (c) 2018 onlyAngelia. All rights reserved.
+//
+
+@import UIKit;
+#import "YAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YAAppDelegate class]));
+    }
+}
